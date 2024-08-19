ef889857bc56330a97c1182b21d5f1be69a70d19
chie-hira <capella.a36@gmail.com>
2024-08-19 10:39:43 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  // 今回はフックとしてuseStateを使用
   const defaultTodos = [
     { id: 1, name: '掃除', completed: false },
     { id: 2, name: '洗濯', completed: false },
     { id: 3, name: '料理', completed: false },
   ];
   const [todos, setTodos] = useState(defaultTodos); 
+
+  // create
+  // inputの値を取得するためのref
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    // inputも文字列を取得
+    const inputName = inputRef.current.value;
+    
+    const newTodo = { id: uuidv4(), name: inputName, completed: false };
+    setTodos([...todos, newTodo]);
+
+    inputRef.current.value = '';
+  }
   
   return (
     <> 
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>タスクの追加</button>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleAddTodo}>タスクの追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
     </>
