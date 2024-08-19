01275f4a30a28f0401f02651d64a63e683e06ce3
chie-hira <capella.a36@gmail.com>
2024-08-19 09:39:39 +0900

    作業準備:不要なファイル、コードを削除

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// rootをレンダリング
+// public/index.htmlの<div id="root"></div>にAppコンポーネントをレンダリングする
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 厳格なモード、エラーがあれば即表示、脆弱性の防止
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
